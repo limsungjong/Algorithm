e139975ae789be5843f8a627bb08174122558e88
Van <82739304+limsungjong@users.noreply.github.com>
2023-05-30 17:00:25 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 2.39 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        Arrays.sort(numbers);
+        for(int i = 1; i < 10; i++) {
+            if(Arrays.binarySearch(numbers,i) < 0) {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
