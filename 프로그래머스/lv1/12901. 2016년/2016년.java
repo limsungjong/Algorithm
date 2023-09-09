c6de984ece1d9ce02ad9c343b33aab7a1b96e1bd
Van <82739304+limsungjong@users.noreply.github.com>
2023-09-09 10:03:09 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.java
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -3,35 +3,20 @@ import java.time.LocalDate;
 
 class Solution {
     public String solution(int a, int b) {
-        StringBuilder answer = new StringBuilder();
-        LocalDate date = LocalDate.of(2016,a,b);
-        DayOfWeek dayOfWeek = date.getDayOfWeek();
-        
-        int dayOfWeekNumber = dayOfWeek.getValue();
-        
-        switch (dayOfWeekNumber) {
-            case 1 -> {
-                answer.append("MON");
-            }
-            case 2 -> {
-                answer.append("TUE");
-            }
-            case 3 -> {
-                answer.append("WED");
-            }
-            case 4 -> {
-                answer.append("THU");
-            }
-            case 5 -> {
-                answer.append("FRI");
-            }
-            case 6 -> {
-                answer.append("SAT");
-            }
-            case 7 -> {
-                answer.append("SUN");
+        String[] dayOfWeek = {"SUN","MON","TUE","WED","THU","FRI","SAT"};
+        String answer = "";
+        int[] months = {0, 31,29,31,30,31,30,31,31,30,31,30,31};
+        int curDay = 6;
+        for(int month = 1; month <= 12; month++) {
+        int monthOfInDay = months[month];    
+            for(int day = 1; day <= monthOfInDay; day++) {
+                if(month == a && day == b) {
+                    answer = dayOfWeek[curDay - 1];
+                    break;
+                }
+                curDay = (curDay % 7) + 1;
             }
         }
-        return answer.toString();
+        return answer;
     }
 }
